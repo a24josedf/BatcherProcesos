d697d3169c5beeb343f423c160e5e3d17af715d5
carlos <carlos@Matebook-Carlos>
2026-03-18 18:42:47 +0100

    First commit

---
+++ src/main/java/com/mycompany/batcherprocesos/BatcherProcesos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.batcherprocesos;
+
+/**
+ *
+ * @author carlos
+ */
+public class BatcherProcesos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
